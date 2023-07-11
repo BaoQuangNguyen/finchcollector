310d28e3a2b50db8403e9aee9add176bc18ab5b0
BaoQuangNguyen <bnguyen0239@gmail.com>
2023-07-11 15:55:32 -0500

    Finch Lab Part 3

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
 # Create your views here.
 
@@ -25,3 +25,11 @@ def finchs_detail(request, finch_id):
 class FinchCreate(CreateView):
     model = Finch
     fields = '__all__'
+
+class FinchUpdate(UpdateView):
+    model = Finch
+    fields = ['breed', 'description', 'age']
+
+class FinchDelete(DeleteView):
+    model = Finch
+    success_url = '/finchs'
